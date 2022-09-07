766348df316218fe0726295a829e1745283930c8
pravinc3798 <pravin.choudhary88@gmail.com>
2022-09-07 11:33:11 +0530

    Added NumberSign class

---
+++ NumberSign.cs
@@ -0,0 +1,17 @@
+﻿namespace ConditionalStatementsD24
+{
+    public class NumberSign
+    {
+        public static void CheckNumber(int number)
+        {
+            if (number == 0)
+            {
+                Console.WriteLine("0 is a NEUTRAL Number");
+                return;
+            }
+
+            var result = (number > 0) ? string.Format("{0} is a POSITIVE Number", number) : string.Format("{0} is a NEGATIVE Number", number);
+            Console.Write("\n>>>>> {0}", result);
+        }
+    }
+}
