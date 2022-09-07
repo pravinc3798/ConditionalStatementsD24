53078c904cd1d5e28ddfa6429882a54b234900d2
pravinc3798 <pravin.choudhary88@gmail.com>
2022-09-07 11:33:16 +0530

    Added PrintAlphabets class

---
+++ PrintAlphabets.cs
@@ -0,0 +1,17 @@
+﻿namespace ConditionalStatementsD24
+{
+    public class PrintAlphabets
+    {
+        public static void Print()
+        {
+            Console.WriteLine();
+            var start = 'a';
+            for (var i = 0; i < 26; i++)
+            {
+                Console.Write("{0} -> ",start);
+                start++;
+            }
+            Console.Write("End \n");
+        }
+    }
+}
