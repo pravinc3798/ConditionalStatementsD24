5e01c4ca19e803982488bdd6a1aaf9ff906ef09c
pravinc3798 <pravin.choudhary88@gmail.com>
2022-09-07 11:33:21 +0530

    Added VowelConsonant class

---
+++ VowelConsonant.cs
@@ -0,0 +1,13 @@
+﻿namespace ConditionalStatementsD24
+{
+    public class VowelConsonant
+    {
+        public static void ChkAlphabet(string input)
+        {
+            var vowels = new string[] { "a", "e", "i", "o", "u" };
+
+            var result = vowels.Contains(input) ? string.Format("{0} is a VOWEL", input) : string.Format("{0} is a CONSONANT", input);
+            Console.Write("\n>>>>> {0}", result);
+        }
+    }
+}
