d0c71100ca1edab46b0f3d983a72e0e7fd242524
pravinc3798 <pravin.choudhary88@gmail.com>
2022-09-07 11:33:05 +0530

    Added FindLargest class

---
+++ FindLargest.cs
@@ -0,0 +1,26 @@
+﻿namespace ConditionalStatementsD24
+{
+    public class FindLargest
+    {
+        public static void UsingNestedCondition(int[] intArray)
+        {
+            if (intArray[0] > intArray[1])
+            {
+                if (intArray[0] > intArray[2])
+                    Console.WriteLine("{0} is the Largest Number", intArray[0]);
+            }
+            else if (intArray[1] > intArray[2])
+                Console.WriteLine("{0} is the Largest Number", intArray[1]);
+            else
+                Console.WriteLine("{0} is the Largest Number", intArray[2]);
+        }
+
+        public static void UsingAccumulator(int[] intArray)
+        {
+            var max = 0;
+            foreach (var num in intArray)
+                max = (num > max) ? num : max;
+            Console.WriteLine("{0} is the Largest Number", max);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿namespace ConditionalStatementsD24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n 1. Vowel Consonant Check \n 2. Positive Negative Check \n 3. Largest among 3 (nested) \n 4. Largest Among 3 (using array.max) \n 5. Print a-z \n 6. Exit \n\n Input the number corresponding to desired program : "); var input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    Console.Write("\nInput an alphabet : "); var character = (Console.ReadLine()).ToLower();
+                    if (character.Length > 1) { Console.WriteLine("\n Invalid Input !! Try Again"); Main(args); }
+                    VowelConsonant.ChkAlphabet(character);
+                    Main(args);
+                    break;
+
+                case "2":
+                    Console.Write("\nInput a number : "); var number = Convert.ToInt32(Console.ReadLine());
+                    NumberSign.CheckNumber(number);
+                    Main(args);
+                    break;
+
+                case "3":
+                    Console.Write("\nInput 3 numbers (separated by space) : "); var strNumber = Console.ReadLine();
+                    string[] stringNumbers = strNumber.Split(' ');
+                    int[] numbers = new int[stringNumbers.Length];
+                    for (var i = 0; i < numbers.Length; i++)
+                        numbers[i] = Convert.ToInt32(stringNumbers[i]);
+
+                    FindLargest.UsingNestedCondition(numbers);
+                    Main(args);
+                    break;
+
+                case "4":
+                    Console.Write("\nInput 3 numbers (separated by space) : "); var strNum = Console.ReadLine();
+                    string[] strNumbers = strNum.Split(' ');
+                    int[] nums = new int[strNumbers.Length];
+                    for (var i = 0; i < nums.Length; i++)
+                        nums[i] = Convert.ToInt32(strNumbers[i]);
+
+                    FindLargest.UsingAccumulator(nums);
+                    Main(args);
+                    break;
+
+                case "5":
+                    PrintAlphabets.Print();
+                    Main(args);
+                    break;
+
+                case "6":
+                    break;
+
+                default:
+                    Main(args);
+                    break;
+            }
+        }
+    }
+}
